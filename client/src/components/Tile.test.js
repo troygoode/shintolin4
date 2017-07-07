3affedbc5f589343b59cb30cd4cb29d9c7e9ea06
Troy Goode <troygoode@gmail.com>
2017-07-07 16:20:18 +0200

    yarn upgrade

--- client/src/components/Tile.test.js
+++ client/src/components/Tile.test.js
@@ -4,7 +4,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import { it } from '../jest'
-import UUT from './Test'
+import UUT from './Tile'
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
